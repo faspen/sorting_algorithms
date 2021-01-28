e71525b103ae9038501a011068854a6baf9bf0fd
faspen <aspensonf@gmail.com>
2021-01-28 19:12:04 +0000

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+* swap - swap two values
+* @i: value 1
+* @j: value 2
+*/
+void swap(int *i, int *j)
+{
+	int tmp;
+
+	tmp = *i;
+	*i = *j;
+	*j = tmp;
+}
+/**
+* bubble_sort - sort integers ascending order
+*
+* @array: array to be sorted
+* @size: size of array
+*
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
