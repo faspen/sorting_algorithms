d7b1eac45b3c32bec524251372809168df884dc8
faspen <aspensonf@gmail.com>
2021-02-01 19:36:46 +0000

    cleanup

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,16 +26,16 @@ void bubble_sort(int *array, size_t size)
 
 	if (size >= 2)
 	{
-	for (i = 0; i < size - 1; i++)
-	{
-		for (j = 0; j < size - i - 1; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < size - i - 1; j++)
 			{
-				swap(&array[j], &array[j + 1]);
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+				}
 			}
 		}
-	}
 	}
 }
